ab66090fa537de544bfccff9d06b10f65942c9dc
AnaisDiGenua <anais.digenua5@gmail.com>
2021-11-05 17:50:25 +0100

    insert variables

--- js/script.js
+++ js/script.js
@@ -3,3 +3,28 @@
 // il prezzo del biglietto è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 // Bonus: effettuate dei controlli sui dati di input
+
+
+// ? prezzo totale del biglietto
+
+//prezzo senza sconto = kilometri * 0.21
+
+
+
+
+//variabile km che si vogliono percorrere
+const kilometri = parseFloat(prompt('inserisci i kilometri della tratta'));
+console.log(kilometri);
+
+//variabile età
+const età = parseInt(prompt('inserisci la tua età'));
+console.log(età);
+
+//variabile prezzo intero
+let prezzoIntero = kilometri * 0.21;
+console.log(prezzoIntero);
+
+
+
+
+
