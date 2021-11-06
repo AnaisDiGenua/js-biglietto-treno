99802ce1210ce27937d39da8620f54af44aa2b3c
AnaisDiGenua <anais.digenua5@gmail.com>
2021-11-06 17:07:08 +0100

    add styles

--- js/script.js
+++ js/script.js
@@ -11,26 +11,43 @@
 
 
 
+// variabile nome
+let nomePasseggero = prompt('inserisci il tuo nome e cognome');
+nomePasseggero = nomePasseggero.toLowerCase().replace(/(?<= )[^\s]|^./g, a => a.toUpperCase());
+document.getElementById('nome').innerHTML = nomePasseggero
+
 
 //variabile km che si vogliono percorrere
 const kilometri = parseFloat(prompt('inserisci i kilometri della tratta'));
+document.getElementById('km').innerHTML = kilometri + ' km'
 // console.log(kilometri);
+
 
 //variabile età
 const età = parseInt(prompt('inserisci la tua età'));
+document.getElementById('età').innerHTML = età
 // console.log(età);
+
 
 //variabile prezzo intero
 let prezzoIntero = parseFloat(kilometri * 0.21).toFixed(2);
-console.log(prezzoIntero);
+// console.log(prezzoIntero);
+
 
 //variabile prezzo minorenni
 let scontoMinorenni = parseFloat(prezzoIntero - (prezzoIntero * 20 / 100)).toFixed(2);
-console.log(scontoMinorenni);
+// console.log(scontoMinorenni);
+
 
 //variabile prezzo0ver65
 let scontoOver65 = parseFloat(prezzoIntero - (prezzoIntero * 40 / 100)).toFixed(2);
-console.log(scontoOver65);
+// console.log(scontoOver65);
+
+
+// variabile codice biglietto random
+let codiceRandom = Math.floor(Math.random() * (29000 - 20000)) + 20000;
+document.getElementById('codice').innerHTML = codiceRandom
+// console.log('il numero casuale è' + codiceRandom);
 
 
 
@@ -39,12 +56,14 @@ console.log(scontoOver65);
 //stampa del biglietto 
 if (età <= 17) {
     console.log('il prezzo del biglietto è ' + scontoMinorenni);
+    document.getElementById('prezzo').innerHTML = scontoMinorenni + '€'
 } else if (età >= 65) {
     console.log('il prezzo del biglietto è ' + scontoOver65);
+    document.getElementById('prezzo').innerHTML = ScontoOver65 + '€'
 } else {
     console.log('il prezzo del biglietto è ' + prezzoIntero);
+    document.getElementById('prezzo').innerHTML = prezzoIntero + '€'
 }
-
 
 
 
